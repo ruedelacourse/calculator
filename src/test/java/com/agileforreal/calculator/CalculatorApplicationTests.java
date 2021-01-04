23defe460ae70b2a7711e903eaef0e61bc510a6c
Thorsten <thorsten@MacBook-Pro.local>
2021-01-04 15:27:07 +0100

    Calculator Source Code

---
+++ src/test/java/com/agileforreal/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agileforreal.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
